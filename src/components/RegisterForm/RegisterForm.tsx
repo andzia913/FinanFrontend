af97d6daf77bf0a6e8d6fafa4e1e420192237bf4
Angelika Szczerbowska <andzia913@gmail.com>
2024-02-13 18:00:25 +0100

    Split logic of registration, RegisterForm is only form, rest of logic is in Register component

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,156 +1,85 @@
+import { useRef } from "react";
 import {
   Typography,
   Link,
   Button,
-  Snackbar,
-  Alert,
   Box,
   TextField,
   Container,
 } from "@mui/material";
-import { useState, useRef } from "react";
-import serverAddress from "../../utils/server";
 
-type RegisterProps = {};
+type RegisterProps = { handleSubmit: Function };
 
-const Register: React.FC<RegisterProps> = () => {
+const Register: React.FC<RegisterProps> = ({ handleSubmit }: RegisterProps) => {
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
   const passwordRepeadRef = useRef<HTMLInputElement>(null);
-  const [success, setSuccess] = useState<boolean>(false);
-  const [alert, setAlert] = useState({ isShown: false, text: "" });
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (passwordRef.current?.value !== passwordRepeadRef.current?.value) {
-      setAlert({
-        isShown: true,
-        text: "Wprowadzone hasła różnią się od siebie",
-      });
-      return;
-    }
-
-    try {
-      const response = await fetch(serverAddress + "/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: nameRef.current?.value,
-          email: emailRef.current?.value,
-          password: passwordRef.current?.value,
-        }),
-      });
-      if (response.status === 201) {
-        setSuccess(true);
-      } else if (response.status === 400) {
-        setAlert({
-          isShown: true,
-          text: "Podano błędne dane. Spróbuj ponownie",
-        });
-      } else if (response.status === 409) {
-        setAlert({
-          isShown: true,
-          text: "Istnieje już uzytkownik przypisany do tego adresu email",
-        });
-      }
-    } catch (err) {
-      if (!err) {
-        setAlert({ isShown: true, text: "Brak odpowiedzi servera" });
-      } else {
-        setAlert({
-          isShown: true,
-          text: "Rejstracja nie powiodło się. Spróbuj ponownie później.",
-        });
-      }
-    }
-  };
 
   return (
     <Container maxWidth="xs">
-      {alert && (
-        <Snackbar
-          open={alert.isShown}
-          autoHideDuration={3000}
-          anchorOrigin={{ vertical: "top", horizontal: "center" }}
-          onClose={() =>
-            setAlert({
-              isShown: false,
-              text: "",
-            })
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        mt={4}
+      >
+        <Typography variant="h4">Rejestracja</Typography>
+        <Box
+          component="form"
+          onSubmit={(e) =>
+            handleSubmit(e, nameRef, emailRef, passwordRef, passwordRepeadRef)
           }
         >
-          <Alert severity="error">{alert.text}</Alert>
-        </Snackbar>
-      )}
-      {success ? (
-        <Box mt={4}>
-          <Typography variant="h4">Użytkownik zarejestrowany</Typography>
-          <Typography>
-            <Link href="/login">Zaloguj się</Link>
-          </Typography>
+          <TextField
+            inputRef={nameRef}
+            type="text"
+            label="Imię"
+            id="username"
+            autoComplete="off"
+            required
+            fullWidth
+            margin="normal"
+          />
+
+          <TextField
+            inputRef={emailRef}
+            type="email"
+            label="email"
+            id="email"
+            autoComplete="off"
+            required
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            inputRef={passwordRef}
+            type="password"
+            id="password"
+            label="hasło"
+            required
+            fullWidth
+            margin="normal"
+          />
+
+          <TextField
+            inputRef={passwordRepeadRef}
+            type="password"
+            id="confirm_pwd"
+            label="Powtórz hasło"
+            required
+            fullWidth
+            margin="normal"
+          />
+
+          <Button type="submit">Zarejestruj się</Button>
         </Box>
-      ) : (
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-          mt={4}
-        >
-          <Typography variant="h4">Rejestracja</Typography>
-          <Box component="form" onSubmit={handleSubmit}>
-            <TextField
-              inputRef={nameRef}
-              type="text"
-              label="Imię"
-              id="username"
-              autoComplete="off"
-              required
-              fullWidth
-              margin="normal"
-            />
-
-            <TextField
-              inputRef={emailRef}
-              type="email"
-              label="email"
-              id="email"
-              autoComplete="off"
-              required
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              inputRef={passwordRef}
-              type="password"
-              id="password"
-              label="hasło"
-              required
-              fullWidth
-              margin="normal"
-            />
-
-            <TextField
-              inputRef={passwordRepeadRef}
-              type="password"
-              id="confirm_pwd"
-              label="Powtórz hasło"
-              required
-              fullWidth
-              margin="normal"
-            />
-
-            <Button type="submit">Zarejestruj się</Button>
-          </Box>
-          <Typography>Masz już konto?</Typography>
-          <Link href="/login">Zaloguj się</Link>
-        </Box>
-      )}
+        <Typography>Masz już konto?</Typography>
+        <Link href="/login">Zaloguj się</Link>
+      </Box>
     </Container>
   );
 };
--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,13 +1,79 @@
-import React from 'react';
-import { Container } from '@mui/material';
-import NavBar from '../../components/NavBar/Navbar';
-import RegisterForm from '../../components/RegisterForm/RegisterForm';
+import React, { useState } from "react";
+import { Box, Container, Link, Typography } from "@mui/material";
+import NavBar from "../../components/NavBar/Navbar";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import serverAddress from "../../utils/server";
 
 const Register = () => {
+  const [success, setSuccess] = useState<boolean>(false);
+  const [alert, setAlert] = useState({ isShown: false, text: "" });
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+    nameRef: React.RefObject<HTMLInputElement>,
+    emailRef: React.RefObject<HTMLInputElement>,
+    passwordRef: React.RefObject<HTMLInputElement>,
+    passwordRepeadRef: React.RefObject<HTMLInputElement>
+  ) => {
+    e.preventDefault();
+    if (passwordRef.current?.value !== passwordRepeadRef.current?.value) {
+      setAlert({
+        isShown: true,
+        text: "Wprowadzone hasła różnią się od siebie",
+      });
+      return;
+    }
+    try {
+      const response = await fetch(serverAddress + "/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: nameRef.current?.value,
+          email: emailRef.current?.value,
+          password: passwordRef.current?.value,
+        }),
+      });
+      if (response.status === 201) {
+        setSuccess(true);
+      } else if (response.status === 400) {
+        setAlert({
+          isShown: true,
+          text: "Podano błędne dane. Spróbuj ponownie",
+        });
+      } else if (response.status === 409) {
+        setAlert({
+          isShown: true,
+          text: "Istnieje już uzytkownik przypisany do tego adresu email",
+        });
+      }
+    } catch (err) {
+      if (!err) {
+        setAlert({ isShown: true, text: "Brak odpowiedzi serwera" });
+      } else {
+        setAlert({
+          isShown: true,
+          text: "Rejestracja nie powiodło się. Spróbuj ponownie później.",
+        });
+      }
+    }
+  };
   return (
     <Container disableGutters={true}>
-      <NavBar/>
-      <RegisterForm/>
+      <NavBar />
+      {alert.isShown && <ErrorMessage alert={alert} setAlert={setAlert} />}
+      {success ? (
+        <Box mt={4}>
+          <Typography variant="h4">Użytkownik zarejestrowany</Typography>
+          <Typography>
+            <Link href="/login">Zaloguj się</Link>
+          </Typography>
+        </Box>
+      ) : (
+        <RegisterForm handleSubmit={handleSubmit} />
+      )}
     </Container>
   );
 };
