ef17308135bb6128a4ec9cf65ab29c78cad065b4
Angelika Szczerbowska <andzia913@gmail.com>
2024-02-13 15:31:46 +0100

    Changed server to local host during development

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,3 +1,4 @@
-const serverAddress = "https://localhost:5000";
+// const serverAddress = "http://vps-19591a52.vps.ovh.net:80";
+const serverAddress = "http://localhost:5000";
 
 export default serverAddress;
