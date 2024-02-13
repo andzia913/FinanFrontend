f1dfd379d4618558c272557521c00966a1717508
Angelika Szczerbowska <andzia913@gmail.com>
2024-02-13 15:31:25 +0100

    Disable mkcert, deleted unused .env file, witch should not be ever commited.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
-
-// export default defineConfig({
-//   server: { https: true }, // Not needed for Vite 5+
-//   plugins: [mkcert()],
-// });
+// import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig(() => {
   return {
@@ -13,6 +8,9 @@ export default defineConfig(() => {
       outDir: "build",
       server: { https: true },
     },
-    plugins: [react(), mkcert()],
+    plugins: [
+      react(),
+      //  mkcert()
+    ],
   };
 });
